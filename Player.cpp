3a31b177f15c021ae83b8c4143334e687b8a734f
grey-gp <grey.grahamp@gmail.com>
2021-11-19 20:34:23 -0500

    Removed code from player class

--- Player.cpp
+++ Player.cpp
@@ -1,19 +1,7 @@
 #include "Player.h"
 
-// Player::Player(Texture2D texture, int frames)
-// {
-//     // characterSprite = texture;
-//     // this->frames = frames;
-//     // frameHeight = (float)characterSprite.height/frames;
-//     // frameRec = Rectangle{0.f, currentFrame * frameHeight, (float)characterSprite.width, frameHeight};
-//     Character(texture, frames);
-// }
-
-
-
 void Player::HandleMovement()
 {
-
     HandleAnim();
 
     if (IsKeyDown(KEY_RIGHT))
